b1bca49839db65baa45b43662b9a4735b4701a09
vaishu1818 <vaishnavimahajan074@gmail.com>
2025-05-20 23:49:53 +0530

    done

---
+++ customer-feedback/src/main/java/com/example/customer_feedback/CustomerFeedbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.customer_feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerFeedbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerFeedbackApplication.class, args);
+	}
+
+}
---
+++ customer-feedback/src/test/java/com/example/customer_feedback/CustomerFeedbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.customer_feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerFeedbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
